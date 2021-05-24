dcf81c162985251182945cf41c5d397c3383ce00
arash-sadeghi <arash.sadeghi1997@gmail.com>
2021-05-25 02:38:43 +0300

    Successfull test on actual data with ANN

---
+++ 24 may 2021 ANN test/ANNclass.py
@@ -0,0 +1,91 @@
+import os
+import sys
+print(sys.version_info.major, sys.version_info.minor)
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+# Network--------------------------------------------------------------------------------------------------------
+class Network(nn.Module):
+    def __init__(self, input_dims, fc1_dims, fc2_dims,out_dim, name,learning_rate = 0.001,chkpt_dir=__file__):
+        super(Network, self).__init__()
+        self.device=T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        print("[+] device {}".format(self.device))
+        self.input_dims = input_dims
+        self.out_dim=out_dim
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.name = name
+        self.learning_rate=learning_rate
+        self.checkpoint_dir = chkpt_dir
+        # self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_ddpg')
+        self.checkpoint_file = os.path.join(os.getcwd(), name+'_ddpg')
+
+        ''' network for angle '''
+        self.fc1a = nn.Linear(self.input_dims, self.fc1_dims)
+        self.fc2a = nn.Linear(self.fc1_dims, self.fc2_dims)
+        # self.fc3a = nn.Linear(self.fc2_dims, self.out_dim)
+
+        self.bn0a = nn.LayerNorm(self.input_dims)
+        self.bn1a = nn.LayerNorm(self.fc1_dims)
+        self.bn2a = nn.LayerNorm(self.fc2_dims)
+
+        self.mua = nn.Linear(self.fc2_dims, self.out_dim)
+
+        f2 = 1./np.sqrt(self.fc2a.weight.data.size()[0])
+        self.fc2a.weight.data.uniform_(-f2, f2)
+        self.fc2a.bias.data.uniform_(-f2, f2)
+
+        f1 = 1./np.sqrt(self.fc1a.weight.data.size()[0])
+        self.fc1a.weight.data.uniform_(-f1, f1)
+        self.fc1a.bias.data.uniform_(-f1, f1)
+
+        f3 = 0.003
+        self.mua.weight.data.uniform_(-f3, f3)
+        self.mua.bias.data.uniform_(-f3, f3)
+
+        ''' done '''
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        # self.loss_func = F.mse_loss()
+
+    def forward(self, input):
+        a=input
+        # a=self.bn0a(a)
+
+        a = self.fc1a(a)
+        a = self.bn1a(a) # batch norms incredibally increase accuracy
+        a = F.relu(a)
+        a = self.fc2a(a)
+        a = self.bn2a(a)
+        a = F.relu(a)
+        a=self.mua(a)
+        # a = F.relu(a)
+        # a = T.sigmoid(self.mua(a)) # to bound action output
+        self.result=a
+        return self.result
+
+    def backward_prop(self,target):
+        self.optimizer.zero_grad()
+        # loss = self.loss_func(target, self.result) # critic_value is derived from normal net and target is the reward computed by bellman
+        # self.loss_func = F.mse_loss()
+
+        loss = F.mse_loss(target, self.result) # critic_value is derived from normal net and target is the reward computed by bellman
+        loss.backward()
+        self.optimizer.step()
+        ''' so now critic network will approach to the actual rewards+gamma*future rewards '''
+
+
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+    def save_best(self):
+        print('... saving best checkpoint ...')
+        checkpoint_file = os.path.join(self.checkpoint_dir, self.name+'_best')
+        T.save(self.state_dict(), checkpoint_file)
---
+++ 24 may 2021 ANN test/TrainableDataTest.py
@@ -0,0 +1,85 @@
+import os
+import sys
+print(sys.version_info.major, sys.version_info.minor)
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+from ANNclass import Network
+import cv2 as cv
+from math import exp
+
+def m2px(inp):
+    return int(inp*512/2)
+
+def generateBackground(Lx,Ly,cueRadius,visibleRaduis=None):
+    Lxpx=m2px(Lx)
+    Lypx=m2px(Ly)
+    R=m2px(cueRadius)
+    def gauss(x):
+        a = 1.0 # amplititude of peak
+        b = Lxpx/2.0 # center of peak
+        c = Lxpx/11# standard deviation
+        return a*exp(-((x-b)**2)/(2*(c**2)))
+    im=np.zeros((Lxpx,Lypx))
+    for i in range(R):
+        cv.circle(im,(int((Lypx/2)),int(Lxpx/2)),i,gauss(Lxpx/2-i),2)
+
+
+    ''' until here dim(x)>dim(y). after here it changes '''
+    im=cv.rotate(im, cv.ROTATE_90_CLOCKWISE)
+
+
+    # for i in self.QRloc.values():
+    #     cv.circle(im,tuple(i),10,(255,255,255),-1)
+    #     cv.circle(im,tuple(i),visibleRaduis,(255,255,255),1)
+    im=255-255*im
+    '''writing and reading back the image to have a 3 channel image with pixels between 0-255'''
+    cv.imwrite("BackgroundGeneratedBySim.png",im)
+    im=cv.imread("BackgroundGeneratedBySim.png")
+    # im=cv.rectangle(im,(0,0),(Lxpx,Lypx),(0,255,0),3)
+    return im
+
+Lx=1
+Ly=2
+cueRadius=0.25
+im=generateBackground(Lx,Ly,cueRadius)
+
+sample_number=im.shape[0]*im.shape[1]//300
+X=np.random.randint(low=0,high=im.shape[0],size=sample_number)
+Y=np.random.randint(low=0,high=im.shape[1],size=sample_number)
+im_part=np.zeros(im.shape)
+cords=np.array(list(zip(X,Y)))
+im_part[cords.T[0],cords.T[1],:]=im[cords.T[0],cords.T[1],:]
+im=im[:,:,0] # grey
+im_part=im_part[:,:,0] # grey
+""" for imshow cast the matrix between 0 and 1 by im/255 """
+im_part=im_part/255 # normalized
+im=im/255 # normalized
+
+net = Network(input_dims=2, fc1_dims=5, fc2_dims=8,out_dim=1, name='im_estimator')
+interupt= True
+epoch=10000//2
+ep=0
+
+# ind1=cords.T[0]/np.max(cords.T[0])
+# ind2=cords.T[1]/np.max(cords.T[1])
+ind=np.zeros(cords.shape)
+ind[:,0]=cords[:,0]/im.shape[0] # normalizing
+ind[:,1]=cords[:,1]/im.shape[1] # normalizing
+
+target=im[cords.T[0],cords.T[1]]
+target=target.reshape(target.shape[0],1)
+
+while ep<=epoch and interupt:
+    res=net.forward(T.tensor(ind).float())
+    net.backward_prop(T.tensor(target).float())
+    err_v=(T.tensor(target)-res).detach().numpy()
+    if not ep%100: print('[+] ep ',ep,'error ',sum(err_v**2)/len(err_v))
+    ep+=1
+
+net.save_checkpoint()
+
+print('hi')
